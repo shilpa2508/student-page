0ec6a9f5169c8e133a645df002e6f1113dc75b4a
Igor Steffen <igorsteffen@hotmail.com>
2025-02-11 17:15:15 -0500

    first commit

---
+++ app/menu.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu").innerHTML = `
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="create-student.html">Add Student</a></li>
+                <li><a href="list-students.html">Student List</a></li>
+            </ul>
+        </nav>
+    `;
+});
